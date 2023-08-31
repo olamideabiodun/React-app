258692d6c078d213e2e86149dd740b2966a35735
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2023-08-31 14:07:47 +0300

    command.sh

---
+++ command.sh
@@ -0,0 +1,13 @@
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+# export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# nvm install 14
+# nvm use 14
+# npx create-react-app my-app
+# cd my-app
+# git checkout -b update_logo
+# git add .
+# git commit
+# git remote set-url origin https://github.com/olamideabiodun/blackbird.git
+# git push origin update_logo
+# REPO_URL https://github.com/olamideabiodun/blackbird.git
